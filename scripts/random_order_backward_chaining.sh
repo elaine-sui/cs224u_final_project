f428c0963fc5fd7bd9c8b84210f6ef4c29bca6c1
esui <esui@stanford.edu>
2023-06-02 00:08:19 -0700

    modified random order scripts

--- scripts/random_order_backward_chaining.sh
+++ scripts/random_order_backward_chaining.sh
@@ -1,3 +1,6 @@
-python3 run.py \
-    --prompt_type backward \
-    --randomized_order
+for seed in 1234 5678; do
+    python3 run.py \
+        --prompt_type backward \
+        --randomized_order \
+        --seed $seed
+done
--- scripts/random_order_forward_chaining.sh
+++ scripts/random_order_forward_chaining.sh
@@ -1,3 +1,6 @@
-python3 run.py \
-    --prompt_type forward \
-    --randomized_order
+for seed in 1234 5678; do
+    python3 run.py \
+        --prompt_type forward \
+        --randomized_order \
+        --seed $seed
+done
