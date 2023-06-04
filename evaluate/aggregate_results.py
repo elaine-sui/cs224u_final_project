e3d1198bd548f6fc6f6c53f51199bc61826aea59
esui <esui@stanford.edu>
2023-06-03 23:23:53 -0700

    added more detail to instructions

--- evaluate/aggregate_results.py
+++ evaluate/aggregate_results.py
@@ -3,18 +3,20 @@ import pandas as pd
 
 # TODO: aggregate consistency results:
 """
+Baseline (1234, 5678, 910)
 Forward + backward
 Forward negated + unnegated
 Backward negated + unnegated
 Forward randomized (1234, 5678, 910)
 Backward randomized (1234, 5678, 910)
 
-How to aggregate CoT?
+How to aggregate CoT? (write each one as a separate function -- helpful for evaluation/analysis)
  - Intersection of both CoTs (converted so that they are comparable)
  - Union of both CoTs (converted so that they are comparable)
- - Choose the longer one of the two
+ - Choose the longest one
 
-How to aggregate answer?
- - If agree, take that answer
- - If disagreement, choose answer corresponding with the longest CoT
+How to aggregate answer? (write each one as a separate function -- helpful for evaluation/analysis)
+ - Take majority answer
+ - [Only applies if aggregating 2 results] 
+    - If disagreement, choose answer corresponding with the longest CoT
 """
