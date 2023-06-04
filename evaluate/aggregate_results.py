56df0d33c84ba76a47e78f3c0102e4cf0cb376b1
esui <esui@stanford.edu>
2023-06-03 23:31:16 -0700

    added more detail to instructions

--- evaluate/aggregate_results.py
+++ evaluate/aggregate_results.py
@@ -11,12 +11,14 @@ Forward randomized (1234, 5678, 910)
 Backward randomized (1234, 5678, 910)
 
 How to aggregate CoT? (write each one as a separate function -- helpful for evaluation/analysis)
- - Intersection of both CoTs (converted so that they are comparable)
- - Union of both CoTs (converted so that they are comparable)
+ - Intersection of all CoTs (converted so that they are comparable)
+ - Union of all CoTs (converted so that they are comparable)
  - Choose the longest one
 
 How to aggregate answer? (write each one as a separate function -- helpful for evaluation/analysis)
  - Take majority answer
  - [Only applies if aggregating 2 results] 
-    - If disagreement, choose answer corresponding with the longest CoT
+    - If disagreement, 2 choices:
+        - 1) Must choose answer: choose answer corresponding with the longest CoT
+        - 2) Can output "I don't know"
 """
