9ac5f21dcbb4385b4969bae37052e7e6645de92d
esui <esui@stanford.edu>
2023-06-06 22:47:28 -0700

    added evaluation

--- evaluate/metrics.py
+++ evaluate/metrics.py
@@ -1,11 +1,15 @@
 from typing import List
 import numpy as np
 
-def strict_acc(preds: List[str], labels: List[str]):
+# def strict_acc(preds: List[str], labels: List[str]):
+#     # Strict accuracy
+#     num_correct = sum(np.array(preds) == np.array(labels))
+#     acc = num_correct / len(preds)
+#     return acc
+
+def strict_acc(pred, label):
     # Strict accuracy
-    num_correct = sum(np.array(preds) == np.array(labels))
-    acc = num_correct / len(preds)
-    return acc
+    return int(pred == label)
 
 def hallucination_count(pred_cot: List[str], premise: List[str], query: str):
     # number of steps that are hallucinated
@@ -18,10 +22,23 @@ def skipped_steps_count(pred_cot, gold_cot):
     return len(set(gold_cot) - set(pred_cot))
 
 def cot_precision(pred_cot, gold_cot):
+    # pred_cot, gold_cot are List[str] with one item
+    if len(pred_cot) == 0: # note: some predicted cots are empty if unparseable or no intersection
+        return 0
+    pred_cot = pred_cot[0]
+    gold_cot = gold_cot[0]
     correct_steps = set(pred_cot).intersection(set(gold_cot))
     return len(correct_steps) / len(pred_cot)
 
 def cot_recall(pred_cot, gold_cot):
+    # pred_cot, gold_cot are List[str] with one item
+    if len(pred_cot) == 0:
+        return 0
+    pred_cot = pred_cot[0]
+    gold_cot = gold_cot[0]
     correct_steps = set(pred_cot).intersection(set(gold_cot))
     return len(correct_steps) / len(gold_cot)
+
+def f1_score(precision, recall):
+    return (2 * precision * recall) / (precision + recall + 1e-7)
         
